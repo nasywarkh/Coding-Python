ce9e9ea11420735ad36278f4bd5248b9084eeb34
nasywarkh <nasywarkh13@gmail.com>
2024-03-26 10:22:24 +0700

    Data Type

---
+++ Data-Type.py
@@ -0,0 +1,36 @@
+name = "Ahmad"
+print(type(name))
+
+age = 17
+print(type(age))
+
+tinggi_badan = 159.7
+print(type(tinggi_badan))
+
+animals = ["Panda", "Cat", "Lion"]
+print(type(animals))
+
+is_active = True
+print(type(is_active))
+
+car_model = None
+print(type(car_model))
+
+print(20 * "=")
+num_int = 140
+num_float = float(num_int)
+print("Number integer", num_int)
+print("Number float", num_float)
+
+num_float2 = 132.56
+num_int2 = int(num_float2)
+print("Number float", num_float2)
+print("Number integer", num_int2)
+
+num_str = str(num_float2)
+print("Number String of Float", num_str)
+
+text = "String"
+text_list = list(text)
+print(text_list)
+
---
+++ Python-Data-Types.py
@@ -0,0 +1,13 @@
+Name = "Nasywa"
+Age = 18
+Height = 150.0
+Graduated = True
+Married = None
+
+Data_student = [Name, Age, Height, Graduated, Married]
+print(Data_student)
+print(type(Data_student[0]))
+print(type(Data_student[1]))
+print(type(Data_student[2]))
+print(type(Data_student[3]))
+print(type(Data_student[4]))
