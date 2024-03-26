82034e7d5f72f388e62e4a9a3cfba2a9b26c5209
nasywarkh <nasywarkh13@gmail.com>
2024-03-26 11:00:39 +0700

    Number Operators

---
+++ Number-Operators1.py
@@ -0,0 +1,60 @@
+a = 3 + 5j
+print(a)
+print(type(a))
+
+b = -6j
+print(b)
+print(type(b))
+
+number1 = 13
+number2 = 2
+
+add = number1 + number2
+print(f'{number1} + {number2} = {add}')
+print(type(add))
+
+sub = number1 - number2
+print(f'{number1} - {number2} = {sub}')
+print(type(sub))
+
+multipli = number1 * number2
+print(f'{number1} x {number2} = {multipli}')
+print(type(multipli))
+
+div = number1 / number2
+print(f'{number1} / {number2} = {div}')
+print(type(div))
+
+floor_div = number1 // number2
+print(f'{number1} // {number2} = {floor_div}')
+print(type(floor_div))
+
+floor_div = 15 // 8
+print(f'15 // 8 = {floor_div}')
+print(type(floor_div))
+
+expo = number1 ** 4
+print(f'{number1} ^ 4 = {expo}')
+print(type(expo))
+
+mod = 15 % 2
+print("15 % 2 =", mod)
+print(type(mod))
+
+print(20 * "=")
+print("Assigment Operator")
+a = 56
+a += 4 # a = a + 4
+print(a)
+a -= 4 # a = a - 4
+print(a)
+a *= 2 # a = a * 4
+print(a)
+a /= 4 # a = a / 4
+print(a)
+a //= 4 # a = a // 4
+print(a)
+a **= 2 # a = a ** 4
+print(a)
+a %= 4 # a = a % 4
+print(a)
---
+++ Simple-Calculator.py
@@ -0,0 +1,17 @@
+print("Simple Calculator")
+print(50 * "=")
+
+angka1 = float(input("Masukkan angka pertama :"))
+angka2 = float(input("Masukkan angka kedua :"))
+
+add = angka1 + angka2
+print(f'{angka1} + {angka2} = {add}')
+
+sub = angka1 - angka2
+print(f'{angka1} - {angka2} = {sub}')
+
+multipli = angka1 * angka2
+print(f'{angka1} * {angka2} = {multipli}')
+
+div = angka1 / angka2
+print(f'{angka1} / {angka2} = {div}')
