2e61f3bbb39464cce2159f26b0c30b7179f9c046
nasywarkh <nasywarkh13@gmail.com>
2024-04-05 10:54:51 +0700

    Factorial

--- Factorial-Calculator.py
+++ Factorial-Calculator.py
@@ -1,8 +1,9 @@
 def factorial(number):
-    if number <= 0:
+    if number <= 1:
         return 1
     else:
         result = number * factorial(number-1)
+        print(result)
         return result
     
 print(factorial(int(input("Enter your number : "))))
