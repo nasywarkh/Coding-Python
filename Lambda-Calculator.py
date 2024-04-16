80dd7bc6ad58a2179176bba38b15f00533796a3d
nasywarkh <nasywarkh13@gmail.com>
2024-04-16 10:32:19 +0800

    Lambda Function

---
+++ Lambda-Calculator.py
@@ -0,0 +1,14 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+addition = lambda x,y : x + y
+print(addition(x,y))
+
+substraction = lambda x,y : x - y
+print(substraction(x,y))
+
+multiplication = lambda x,y : x * y
+print(multiplication(x,y))
+
+division = lambda x,y : x / y
+print(division(x,y))
---
+++ Lambda-Function.py
@@ -0,0 +1,34 @@
+lambda name : print("Hello", name)
+
+hello = lambda name, age : print ("Hello", name, age, "years old")
+
+hello("Jack", 20)
+hello("Nur", 21)
+
+
+(lambda name : print("Hello", name))("Ahmad")
+(lambda name : print("Hello", name))("Karim")
+
+is_even = lambda n : print("Yes") if n % 2 == 0 else print("No")
+is_even(4)
+is_even(7)
+
+addition = lambda x,y : x + y
+print(addition(7,3))
+
+substraction = lambda x,y : x - y
+print(substraction(7,3))
+
+multiplication = lambda x,y : x * y
+print(multiplication(7,3))
+
+division = lambda x,y : x / y
+print(division(7,3))
+
+
+
+
+
+
+
+
